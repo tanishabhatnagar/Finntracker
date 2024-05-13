d22228335db1c30759346eeaccc2b6edb1c079b6
Tanisha <tanishabhatnagar484@gmail.com>
2024-05-13 14:26:12 +0530

    added backend

---
+++ backend/controllers/expense.js
@@ -0,0 +1,59 @@
+const ExpenseSchema=require('../models/ExpenseModel')
+
+
+exports.addExpense=async(req,res)=>{
+   const{title,amount,category,description,date}= req.body
+
+   const income=ExpenseSchema({
+    title,
+    amount,
+    category,
+    description,
+    date
+   })
+
+   try{
+    if(!title|| !category ||!description||!date){
+        return res.status(400).json({message:'All fields are required'})
+    }
+
+    if(amount<=0 || amount==='number'){
+        return res.status(400).json({message:'Amount must be positive number'})
+    }
+
+    await income.save()
+    res.status(200).json({message:'expense added'})
+
+
+   }
+   catch(error){
+    res.status(500).json({message:'server error'})
+
+
+   }
+
+   console.log(income);
+}
+
+exports.getExpense=async(req,res)=>{
+    try{
+const incomes=await ExpenseSchema.find().sort({createdAt:-1})
+res.status(200).json(incomes)
+    }
+    catch(error){
+        res.status(500).json({message:'server error'})
+    }
+}
+
+exports.deleteExpense=async(req,res)=>{
+   const {id}=req.params;
+
+   ExpenseSchema.findByIdAndDelete(id)
+   .then((income)=>{
+    res.status(200).json({message:'expense deleted'})
+})
+   .catch((error)=>{
+    res.status(500).json({message:'server error'})
+    
+   })
+}
